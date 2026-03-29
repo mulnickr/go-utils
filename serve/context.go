44a6cb0b5fc7ed09891a4efcec4917da64e76f1c
Ryan Mulnick <guitartheoryapp@gmail.com>
2026-03-28 23:19:23 -0400

    Cookie handling

--- serve/context.go
+++ serve/context.go
@@ -30,6 +30,14 @@ func (c *Context) SetHeader(key, value string) {
 	c.r.Header.Set(key, value)
 }
 
+func (c *Context) Cookie(key string) (*http.Cookie, error) {
+	return c.r.Cookie(key)
+}
+
+func (c *Context) SetCookie(cookie *http.Cookie) {
+	http.SetCookie(c.w, cookie)
+}
+
 func (c *Context) Query(key string) string {
 	return c.r.URL.Query().Get(key)
 }
