913c8f479bc1943bad3bc6e4562d4d7b61cc45a2
Ryan Mulnick <guitartheoryapp@gmail.com>
2026-05-03 00:05:21 -0400

    Remove logging in favor of internal slog package and properly isolate middlewares for groups

--- serve/group.go
+++ serve/group.go
@@ -7,29 +7,46 @@ package serve
 type Group struct {
 	w *Serve
 
-	basePath string
+	basePath    string
+	middlewares []Middleware
 }
 
-func (g *Group) GET(pattern string, handler HandlerFunc) {
-	g.w.GET(g.basePath+pattern, handler)
-}
-
-func (g *Group) POST(pattern string, handler HandlerFunc) {
-	g.w.POST(g.basePath+pattern, handler)
-}
-
-func (g *Group) PUT(pattern string, handler HandlerFunc) {
-	g.w.PUT(g.basePath+pattern, handler)
-}
-
-func (g *Group) DELETE(pattern string, handler HandlerFunc) {
-	g.w.DELETE(g.basePath+pattern, handler)
+func (g *Group) chain(handler Handler) Handler {
+	for i := len(g.middlewares) - 1; i >= 0; i-- {
+		handler = g.middlewares[i](handler)
+	}
+	return handler
 }
 
 func (g *Group) Use(middleware ...Middleware) {
 	g.w.Use(middleware...)
 }
 
+func (g *Group) Handle(pattern string, handler Handler, methods ...string) {
+	g.w.Handle(g.basePath+pattern, g.chain(handler), methods...)
+}
+
+func (g *Group) GET(pattern string, handler HandlerFunc) {
+	g.Handle(pattern, handler, "GET")
+}
+
+func (g *Group) POST(pattern string, handler HandlerFunc) {
+	g.Handle(pattern, handler, "POST")
+}
+
+func (g *Group) PUT(pattern string, handler HandlerFunc) {
+	g.Handle(pattern, handler, "PUT")
+}
+
+func (g *Group) DELETE(pattern string, handler HandlerFunc) {
+	g.Handle(pattern, handler, "DELETE")
+}
+
 func (g *Group) Group(path string) *Group {
-	return g.w.Group(g.basePath + path)
+	child := &Group{
+		w:        g.w,
+		basePath: g.basePath + path,
+	}
+	child.middlewares = append(child.middlewares, g.middlewares...)
+	return child
 }
